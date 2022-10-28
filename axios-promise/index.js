8af71d122e6d263d7621938570c4cc09141942c2
Maksim Plysheuski <plysheuski.maksim@gmail.com>
2022-10-28 18:42:33 +0300

    axios / promise

---
+++ axios-promise/index.js
@@ -0,0 +1,18 @@
+import {getImages} from "./service";
+
+const resultBlock = document.querySelector("#result")
+const pageNumberEl = document.querySelector("#page-number")
+const clickMeButton = document.querySelector("#click-me")
+
+clickMeButton.addEventListener("click", () => {
+    const promise = getImages(pageNumberEl.value)
+    promise.then()
+})
+
+function onDataReceived(array) {
+    array.forEach(el => {
+        const img = document.createElement("img")
+        img.src = el.thumbnail;
+        document.querySelector("#result").appendChild(img)
+    })
+}
---
+++ axios-promise/service.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export function getImages(pageNumber) {
+    const promise = axios.get(`https://repetitora.net/api/JS/Images?page=${pageNumber}&count=1`)
+    return promise.then((response) => {
+        return response.data
+    })
+}
